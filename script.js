5d8b3928201f59ce3853a88f60e708313209dec4
Ashraful Alam Nur <109540177+Ashrafulalamnur@users.noreply.github.com>
2026-01-01 21:51:28 +0600

    formate all

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ let category;
 let meter;
 let BMI;
 function feetToMeter(feet) {
-    return feet * 0.3048;
+  return feet * 0.3048;
 }
 function calculateBMI() {
-    let weight = document.querySelector("#input_weight").value;
-    let feet = document.querySelector("#input_height").value;
-    if (weight <= 0 || feet <= 0) {
-        return (result.innerText = "Enter valid values!");
-    }
-    let height = feetToMeter(feet);
-    BMI = weight / (height * height);
-    if (BMI < 18.5) {
-        category = "Underweight";
-    } else if (BMI > 18.5 && BMI <= 24.9) {
-        category = "Normal";
-    } else if (BMI >= 25 && BMI <= 29.9) {
-        category = "Overweight";
-    } else {
-        category = "Obese";
-    }
-    result.innerText = `Your BMI is ${Math.round(BMI)}, So you are ${category}`;
+  let weight = document.querySelector("#input_weight").value;
+  let feet = document.querySelector("#input_height").value;
+  if (weight <= 0 || feet <= 0) {
+    return (result.innerText = "Enter valid values!");
+  }
+  let height = feetToMeter(feet);
+  BMI = weight / (height * height);
+  if (BMI < 18.5) {
+    category = "Underweight";
+  } else if (BMI > 18.5 && BMI <= 24.9) {
+    category = "Normal";
+  } else if (BMI >= 25 && BMI <= 29.9) {
+    category = "Overweight";
+  } else {
+    category = "Obese";
+  }
+  result.innerText = `Your BMI is ${Math.round(BMI)}, So you are ${category}`;
 }
